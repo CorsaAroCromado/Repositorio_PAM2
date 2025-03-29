3636a83ec56e61d5f1e96a42375bae730f8e710f
Bruno Henrique Pires Maia <165274677+CorsaAroCromado@users.noreply.github.com>
2025-03-29 17:26:23 +0000

    saved

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
-import { List, PaperProvider, TextInput, Button } from 'react-native-paper';
+import { StyleSheet, Text, View, Dimensions, ScrollView, Modal, TouchableOpacity } from 'react-native';
+import { PaperProvider, TextInput, Button } from 'react-native-paper';
+import { Picker } from '@react-native-picker/picker';
 import { useState } from 'react';
 
 export default function App() {
@@ -10,189 +11,207 @@ export default function App() {
   let [numero, setNumero] = useState('');
   let [dados, setDados] = useState({});
   let [complemento, setComplemento] = useState('');
-  const [expanded, setExpanded] = useState(false);
-  const [selectedValue, setSelectedValue] = useState(null);
-
-  // Para tornar o app mais responsivo
-  const { width } = Dimensions.get('window');
-
-  const handlePress = () => { setExpanded(!expanded); };
-
-  const handleItemPress = (value) => {
-    setSelectedValue(value);
-    setExpanded(false);
-  };
+  const [selectedValue, setSelectedValue] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   const buscacep = (xcep) => {
-    // Reseta os dados antes de buscar um novo CEP
-    setDados({});
-    let url = `https://viacep.com.br/ws/${xcep}/json/`;
+    if (xcep.trim() !== '') {
+      setDados({});
+      setSelectedValue('');
+      let url = `https://viacep.com.br/ws/${xcep}/json/`;
 
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((dados) => {
-        setDados(dados);
-        setSelectedValue(dados.uf);
-      })
-      .catch((x) => {
-        console.log(x);
-      });
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((dados) => {
+          if (!dados.erro) {
+            setDados(dados);
+            setSelectedValue(dados.uf);
+          }
+        })
+        .catch((x) => {
+          console.log(x);
+        });
+    }
   };
 
   const handleSubmit = () => {
-    // Limpa todos os campos
     setCep('');
     setNome('');
     setIdade('');
     setNumero('');
     setDados({});
     setComplemento('');
-    setSelectedValue(null);
-    setExpanded(false);
+    setSelectedValue('');
+    setModalVisible(true);
   };
 
   const handleClear = () => {
-    // Limpa todos os campos sem submeter o formulário
     setCep('');
     setNome('');
     setIdade('');
     setNumero('');
     setDados({});
     setComplemento('');
-    setSelectedValue(null);
-    setExpanded(false);
+    setSelectedValue('');
   };
 
   return (
     <PaperProvider>
       <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.text_title}>Insira Sua Informações:</Text>
+        <TextInput label='Nome' mode='outlined' value={nome} onChangeText={setNome} style={styles.input} />
+        <TextInput label='Idade' mode='outlined' value={idade} onChangeText={setIdade} style={styles.input} />
+        
+        <Text style={styles.text_title}>Insira o Seu Endereço:</Text>
         <TextInput
-          label='Nome'
+          label='CEP'
           mode='outlined'
-          value={nome}
-          onChangeText={(value) => setNome(value)}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
-        />
-
-        <TextInput
-          label='Idade'
-          mode='outlined'
-          value={idade}
-          onChangeText={(value) => setIdade(value)}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
-        />
-
-        <TextInput
-          label="CEP"
           value={cep}
           onChangeText={(value) => {
-            setCep(value);
-            buscacep(value);
+            const numericValue = value.replace(/[^0-9]/g, '');
+            setCep(numericValue);
+            buscacep(numericValue);
           }}
-          mode='outlined'
           keyboardType='numeric'
-          onBlur={() => { buscacep(cep) }}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
+          maxLength={8}
+          style={styles.input}
         />
-        <TextInput
-          label='Rua'
-          mode='outlined'
-          value={dados.logradouro || ''}
-          onChangeText={(value) => { setDados({ ...dados, logradouro: value }) }}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
-        />
-
-        <TextInput
-          label='Número'
-          mode='outlined'
-          value={numero}
-          onChangeText={(value) => setNumero(value)}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
-        />
-
-        <TextInput
-          label='Complemento'
-          mode='outlined'
-          value={complemento}
-          onChangeText={(value) => setComplemento(value)}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
-        />
-
-        <TextInput
-          label='Bairro'
-          mode='outlined'
-          value={dados.bairro || ''}
-          onChangeText={(value) => { setDados({ ...dados, bairro: value }) }}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
-        />
-
-        <TextInput
-          label='Cidade'
-          mode='outlined'
-          value={dados.localidade || ''}
-          onChangeText={(value) => { setDados({ ...dados, localidade: value }) }}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
-        />
-
-        <TextInput
-          label='Estado'
-          mode='outlined'
-          value={selectedValue || ''}
-          onChangeText={(value) => { setSelectedValue(value) }}
-          style={[styles.input, { width: width * 0.8 }]} // Largura ajustada
-        />
-
-        <List.Section title='Estado'>
-          <List.Accordion title={selectedValue == null ? 'Selecione o Estado' : selectedValue}
-            expanded={expanded} onPress={handlePress}>
-            <List.Item title="AC" onPress={() => { handleItemPress('AC') }} />
-            <List.Item title="SP" onPress={() => { handleItemPress('SP') }} />
-            <List.Item title="RJ" onPress={() => { handleItemPress('RJ') }} />
-            <List.Item title="BH" onPress={() => { handleItemPress('BH') }} />
-          </List.Accordion>
-        </List.Section>
-
-        <Text style={styles.textoCep}>{dados.logradouro || ''}</Text>
-
+        
+        <TextInput label='Rua' mode='outlined' value={dados.logradouro || ''} onChangeText={(value) => setDados({ ...dados, logradouro: value })} style={styles.input} />
+        <TextInput label='Número' mode='outlined' value={numero} onChangeText={setNumero} style={styles.input} />
+        <TextInput label='Complemento' mode='outlined' value={complemento} onChangeText={setComplemento} style={styles.input} />
+        <TextInput label='Bairro' mode='outlined' value={dados.bairro || ''} onChangeText={(value) => setDados({ ...dados, bairro: value })} style={styles.input} />
+        <TextInput label='Cidade' mode='outlined' value={dados.localidade || ''} onChangeText={(value) => setDados({ ...dados, localidade: value })} style={styles.input} />
+      
+        <Text style={styles.text_title}>Estado:</Text>
+        <Picker
+          selectedValue={selectedValue}
+          onValueChange={(itemValue) => setSelectedValue(itemValue)}
+          style={styles.picker}
+        >
+          <Picker.Item label='Selecione o Estado' value='' />
+          <Picker.Item label='AC' value='AC' />
+          <Picker.Item label='AL' value='AL' />
+          <Picker.Item label='AP' value='AP' />
+          <Picker.Item label='AM' value='AM' />
+          <Picker.Item label='BA' value='BA' />
+          <Picker.Item label='CE' value='CE' />
+          <Picker.Item label='DF' value='DF' />
+          <Picker.Item label='ES' value='ES' />
+          <Picker.Item label='GO' value='GO' />
+          <Picker.Item label='MA' value='MA' />
+          <Picker.Item label='MT' value='MT' />
+          <Picker.Item label='MS' value='MS' />
+          <Picker.Item label='MG' value='MG' />
+          <Picker.Item label='PA' value='PA' />
+          <Picker.Item label='PB' value='PB' />
+          <Picker.Item label='PR' value='PR' />
+          <Picker.Item label='PE' value='PE' />
+          <Picker.Item label='PI' value='PI' />
+          <Picker.Item label='RJ' value='RJ' />
+          <Picker.Item label='RN' value='RN' />
+          <Picker.Item label='RS' value='RS' />
+          <Picker.Item label='RO' value='RO' />
+          <Picker.Item label='RR' value='RR' />
+          <Picker.Item label='SC' value='SC' />
+          <Picker.Item label='SP' value='SP' />
+          <Picker.Item label='SE' value='SE' />
+          <Picker.Item label='TO' value='TO' />
+        </Picker>
+        
         <View style={styles.buttonContainer}>
-          <Button mode="contained" onPress={handleSubmit} style={styles.button}>
-            Enviar
-          </Button>
-          <Button mode="outlined" onPress={handleClear} style={styles.button}>
-            Limpar Campos
-          </Button>
+          <Button mode='contained' onPress={handleSubmit} style={styles.button}>Enviar</Button>
+          <Button mode='outlined' onPress={handleClear} style={styles.button}>Limpar Campos</Button>
         </View>
-
-        <StatusBar style="auto" />
+        <StatusBar style='auto' />
       </ScrollView>
+          {/* Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>Informações enviadas com sucesso!</Text>
+            <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.modalButton}>
+              <Text style={styles.modalButtonText}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+
+
     </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,  // Permite o conteúdo crescer e o ScrollView rolar
-    backgroundColor: '#000',
+    flexGrow: 1,
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 20, // Ajuste da distância das bordas
-    paddingVertical: 10, // Adiciona espaço entre as caixas de texto e os botões
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  picker: {
+    width: '85%',
+    height: 50,
+    marginBottom: 12,
+  },
+  text_title: {
+    color: '#000',
+    fontSize: 18,
+    marginTop: 15,
+    marginBottom: 5,
   },
   input: {
-    marginBottom: 10,
-  },
-  textoCep: {
-    color: 'white',
-  },
-  button: {
-    marginTop: 20,
-    width: '45%',
-    marginHorizontal: 5,
+    marginBottom: 12,
+    width: '85%',
+    borderRadius: 25,
+    backgroundColor: '#fff',
+    paddingHorizontal: 15,
+    height: 45,
+    fontSize: 16,
   },
   buttonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
-    marginTop: 20,
+    justifyContent: 'space-between',
+    width: '85%',
+    marginTop: 15,
+  },
+  button: {
+    borderRadius: 20,
+    height: 45,
+    justifyContent: 'center',
+    width: '48%',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  modalButton: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+  },
+  modalButtonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
